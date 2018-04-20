df289171e14d34bc4cf0cf019a664ce4792ac50c
yj9940 <786436456@qq.com>
2018-04-20 15:45:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/fragmenttest/ChatMainTabFragment.java
@@ -0,0 +1,16 @@
+package com.example.lenovo.fragmenttest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ChatMainTabFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab01,container,false);
+    }
+}
